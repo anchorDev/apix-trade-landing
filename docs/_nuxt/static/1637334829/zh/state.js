6772b7278b2ea5fdb4765c700c4a6d0f80ebb728
im_daniel <shushanyandaniel@gmail.com>
2021-11-19 17:24:25 +0200

    new path #3

--- docs/_nuxt/static/1637334829/zh/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fapix-trade-landing\u002F_nuxt\u002Fstatic\u002F1637334829",layout:"default",error:a,state:{popup:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fzh",config:{_app:{basePath:"\u002Fapix-trade-landing\u002F",assetsPath:"\u002Fapix-trade-landing\u002F_nuxt\u002F",cdnURL:a}}}}(null));
