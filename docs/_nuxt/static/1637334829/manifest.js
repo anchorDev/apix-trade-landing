72208b43be478a06fbca983e69cdc77a46bc85f3
im_daniel <shushanyandaniel@gmail.com>
2021-11-19 17:15:39 +0200

    new path #2

---
+++ docs/_nuxt/static/1637334829/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fzh\u002Fdemo","\u002Fen\u002Fdemo","\u002Fdemo-settings","\u002Fdemo-pays","\u002Fen\u002Fdemo-pays","\u002Fen\u002Fdemo-settings","\u002Fzh\u002Fdemo-pays","\u002Fdemo","\u002Fzh\u002Fdemo-settings","\u002Fzh","\u002Fen"]})
